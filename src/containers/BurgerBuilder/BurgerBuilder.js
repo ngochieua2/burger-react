4de2ca62cf81d71410b6a230231487c271b2d342
Ngoc Hieu Nguyen <ngochieua2@gmail.com>
2021-01-03 20:30:32 +1030

    done in simple testing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,7 +10,7 @@ import withErrorHandler from '../withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
 import axios from '../../axios-orders';
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
     // constructor(props){
     //     supper(props);
     // }
